4a82234ff4effca94059ac36dc581d60e7e39953
dhanidwiputra <dhanidwiputra123@gmail.com>
2017-03-28 23:20:18 +0700

    fix base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gofutsaldev/';
+$config['base_url'] = 'http://31.220.56.83/gofutsaldev/';
 
 /*
 |--------------------------------------------------------------------------
